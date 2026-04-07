a9212fe67dc730dbf30cea136dc91933f7d2bf01
Lalitha947 <lalithancana415@gmail.com>
2026-04-07 09:18:25 +0200

    map-001

--- fleet-management/src/pages/operator/Dashboard.tsx
+++ fleet-management/src/pages/operator/Dashboard.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Map from '../../components/map/Map'
+
 
 const OperatorDashboard: React.FC = () => {
   return (
     <div>
       <h1>Operator Dashboard</h1>
       <div style={{ height: '400px', width: '100%' }}>
-        <Map />
+        
       </div>
       {/* Other dashboard content */}
     </div>
